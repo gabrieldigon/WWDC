fe1129f0f08687d3780731811252fb044503ff75
gabrieldigon <gabrieldiasgonca@gmail.com>
2023-04-03 18:59:05 -0400

    organizesCode

--- NightSky.swiftpm/LampNode.swift
+++ NightSky.swiftpm/LampNode.swift
@@ -3,9 +3,30 @@ import Foundation
 import SpriteKit
 
 class LampNode: SKNode {
-    let form = SKSpriteNode(color: .white, size: CGSize(width: 20, height: 20))
-    let light = SKLightNode()
-   
+    
+    let light : SKLightNode
+    let lamp : SKShapeNode
+    var category : UInt32
+    init(category:UInt32) {
+        
+        self.category = category
+        self.light = SKLightNode()
+        self.light.lightColor = UIColor.yellow
+        self.light.ambientColor = UIColor.gray
+        self.light.shadowColor = UIColor.black
+        self.light.falloff = 1.0
+        self.light.categoryBitMask = category
+        
+        self.lamp = SKShapeNode(circleOfRadius: 20)
+        self.lamp.fillColor = SKColor.yellow
+        
+        super.init()
+            
+        self.addChild(lamp)
+        self.lamp.addChild(light)
+    }
+    
+
     
     required init?(coder aDecoder: NSCoder) {
            fatalError("init(coder:) has not been implemented")
--- NightSky.swiftpm/LpGameScene.swift
+++ NightSky.swiftpm/LpGameScene.swift
@@ -8,38 +8,16 @@ import SpriteKit
 
 class LpGameScene: SKScene {
     var touchLocation = CGPoint()
-    var light = SKLightNode()
-    var lamp = SKShapeNode(circleOfRadius: 20)
+    let lamp = LampNode(category: 1)
     override func didMove(to view: SKView) {
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        light.lightColor = UIColor.yellow
-        light.ambientColor = UIColor.gray
-        light.shadowColor = UIColor.black
-        light.falloff = 1.0
-        light.categoryBitMask = 1
-        lamp.fillColor = SKColor.yellow
         addChild(lamp)
-        lamp.addChild(light)
-        
         let box = SKSpriteNode(color: .white, size: CGSize(width: 50, height: 50))
-//        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
         box.lightingBitMask = 1
         box.shadowCastBitMask = 1
         addChild(box)
         
     }
     
-    //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        guard let touch = touches.first else { return }
-    //        let location = touch.location(in: self)
-    //        let box = SKSpriteNode(color: .white, size: CGSize(width: 50, height: 50))
-    //        box.position = location
-    //        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-    //        box.lightingBitMask = 1
-    //        box.shadowCastBitMask = 1
-    //        addChild(box)
-    //
-    //    }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let location = touches.first?.location(in: self) {
             lamp.position = location
@@ -56,3 +34,7 @@ class LpGameScene: SKScene {
 
 
 
+
+
+
+ 
